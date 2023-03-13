fdbe3a4b41ca5322ddc4f6e2bc3b81a517ca112f
pryority <matthewapryor@gmail.com>
2023-03-13 00:23:22 -0400

    add: extracted miscellaneous components

---
+++ src/components/misc/HamburgerMenuButton.tsx
@@ -0,0 +1,7 @@
+export const HamburgerMenuButton = () => (
+  <svg viewBox="0 0 100 80" width="18" height="18">
+    <rect width="100" height="20" fill="#2C293E"></rect>
+    <rect y="30" width="100" height="20" fill="#2C293E"></rect>
+    <rect y="60" width="100" height="20" fill="#2C293E"></rect>
+  </svg>
+);
